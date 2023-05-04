46f6bd753ee2f638cfa57cf97ec7e49d497b3645
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-05-04 20:56:33 -0300

    Implemented username change.

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,21 @@
+import { useState } from "react"
+
 export default function User() {
+
+    let [username, setUsername] = useState('catanacomics');
+
+    function changeName() {
+        let newUsername = prompt('Atualizar nome de usuário:')
+        if (newUsername !== '') { setUsername(newUsername); }
+    }
+
     return (
         <div class="usuario">
             <img src="assets/img/catanacomics.svg" alt="imagem de perfil" data-test="profile-image" />
             <div class="texto">
                 <span>
-                    <strong><span data-test="name">catanacomics</span></strong>
-                    <ion-icon name="pencil" data-test="edit-name"></ion-icon>
+                    <strong><span data-test="name">{username}</span></strong>
+                    <ion-icon name="pencil" onClick={changeName} data-test="edit-name"></ion-icon>
                 </span>
             </div>
         </div>
